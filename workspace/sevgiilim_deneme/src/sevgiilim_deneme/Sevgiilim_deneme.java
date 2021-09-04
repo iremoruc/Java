7e58a98a4c6f71da7fa883e30ffbe15bd5ccdd1e
iremoruc <irmmorcc@gmail.com>
2021-09-04 22:48:17 +0300

    sevgilim_trial_check2

--- workspace/sevgiilim_deneme/src/sevgiilim_deneme/Sevgiilim_deneme.java
+++ workspace/sevgiilim_deneme/src/sevgiilim_deneme/Sevgiilim_deneme.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sevgiilim_deneme;
 
-/**
- *
- * @author irmmo
- */
 public class Sevgiilim_deneme {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("My dear darling is trying something..." + " and I love him <3s");
+        System.out.println("Although he loves me with all his heart, sometimes i make him sad...");
     }
     
 }
